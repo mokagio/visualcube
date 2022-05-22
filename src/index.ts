b88191dcc66399afdb6f65e0106db7d158981284
Gio Lodi <giovanni.lodi42@gmail.com>
2022-05-22 22:48:39 +1000

    Fix issue were multiple cubes would compound the viewbox scaling
    That was because the options, which the hack for the hidden faces
    projection override, wasn't a true copy, so every iteration would scale
    the already scaled value.

--- src/index.ts
+++ src/index.ts
@@ -85,5 +85,38 @@ function getOptions(defaultOptions: ICubeOptions, extraOptions: string | ICubeOp
     parsedOptions.facelets = parseFaceletDefinitions(parsedOptions.facelets)
   }
 
-  return { ...defaultOptions, ...parsedOptions }
+  // Spread (`...`) is not sufficient to copy because the options have nested values
+  return deepCopy({ ...defaultOptions, ...parsedOptions })
+}
+
+// Hat-tip https://www.delftstack.com/howto/typescript/typescript-cloning-an-object/
+function deepCopy<T>(instance : T) : T {
+    if ( instance == null){
+        return instance;
+    }
+
+    // handle Dates
+    if (instance instanceof Date) {
+        return new Date(instance.getTime()) as any;
+    }
+
+    // handle Array types
+    if (instance instanceof Array){
+        var cloneArr = [] as any[];
+        (instance as any[]).forEach((value)  => {cloneArr.push(value)});
+        // for nested objects
+        return cloneArr.map((value: any) => deepCopy<any>(value)) as any;
+    }
+    // handle objects
+    if (instance instanceof Object) {
+        var copyInstance = { ...(instance as { [key: string]: any }
+        ) } as { [key: string]: any };
+        for (var attr in instance) {
+            if ( (instance as Object).hasOwnProperty(attr))
+                copyInstance[attr] = deepCopy<any>(instance[attr]);
+        }
+        return copyInstance as T;
+    }
+    // handling primitive data types
+    return instance;
 }
