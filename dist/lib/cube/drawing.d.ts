ed757f226586e2e5fe184227eba9db75968e2248
Gio Lodi <giovanni.lodi42@gmail.com>
2022-05-22 22:03:01 +1000

    Build `dist` folder via `npm run build`

--- dist/lib/cube/drawing.d.ts
+++ dist/lib/cube/drawing.d.ts
@@ -5,6 +5,7 @@ import { ICubeOptions } from './options';
 import { Arrow } from './models/arrow';
 export declare function renderCube(container: HTMLElement | string, geometry: CubeGeometry, options: ICubeOptions): void;
 export declare function renderOLLStickers(group: SVG.G, face: Face, stickers: FaceStickers, rotations: FaceRotations, options: ICubeOptions): void;
+export declare function renderHiddenStickers(group: SVG.G, face: Face, stickers: FaceStickers, rotations: FaceRotations, options: ICubeOptions): void;
 /**
  * Generates svg for an arrow pointing from sticker s1 to s2
  */
